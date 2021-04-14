6bc68e9c31fb2ee27d9a556bcb4c1e4c6aed2532
Bohdan <32486313+bkovalevych@users.noreply.github.com>
2021-04-14 21:05:43 +0300

    init

--- AnalogIfranView/MainPage.xaml.cs
+++ AnalogIfranView/MainPage.xaml.cs
@@ -24,6 +24,7 @@ namespace AnalogIfranView
     {
         public MainPage()
         {
+            Windows.Globalization.ApplicationLanguages.PrimaryLanguageOverride = "en-US";
             this.InitializeComponent();
         }
     }
